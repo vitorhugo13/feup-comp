3dd2e6fd06a5cccb0643657217b00d213aaaee54
joanaferreira0011 <joanaferreira0011@gmail.com>
2020-04-24 01:31:55 +0100

    Added exception handling

--- src/SemanticAnalysis.java
+++ src/SemanticAnalysis.java
@@ -1,10 +1,14 @@
 import java.util.ArrayList;
 import descriptors.*;
+import java.io.IOException;
+
 
 class SemanticAnalysis{
+    static private String VOID = "void";
+    static private int MAX_EXCEPTIONS = 10;
     private SymbolTable symbolTable;
     private int exceptionCounter;
-    static private String VOID = "void";
+
 
     public SemanticAnalysis(SymbolTable symbolTable){
         this.symbolTable = symbolTable;
@@ -13,25 +17,35 @@ class SemanticAnalysis{
 
     public void execute(Node node){
         //System.out.println("EXECUTE: " + node.toString());
-        if(node.toString().equals("StaticImport")
-            || node.toString().equals("NonStaticImport")){
+        try {
+            if (node.toString().equals("StaticImport")
+                    || node.toString().equals("NonStaticImport")) {
+            } else if (node.toString().contains("Class")
+                    || node.toString().equals("Method[main]")
+                    || (!node.toString().equals("MethodInvocation")
+                    && node.toString().contains("Method"))) {
+                processNewScope(node);
+            } else if (node.toString().equals("Program")) {
+                processProgram(node);
+            } else if (node.toString().equals("Assign")) {
+                processAssign(node);
+            } else {
+                processChildren(node);
+            }
         }
-        else if(node.toString().contains("Class")
-                || node.toString().equals("Method[main]")
-                || (!node.toString().equals("MethodInvocation")
-                    && node.toString().contains("Method"))){
-           // System.out.println("New Scope: " + node.toString());
-            processNewScope(node);
-        }
-        else if (node.toString().equals("Program")){
-            processProgram(node);
-        }
-        else{
-            processChildren(node);
+        catch (Exception e) {
+            System.err.println("[SEMANTIC ERROR]: " + e.getMessage());
+            exceptionCounter++;
+            if (exceptionCounter >= MAX_EXCEPTIONS) {
+                System.err.println("THERE ARE MORE THAN " + MAX_EXCEPTIONS + " SEMANTIC ERRORS. PROGRAM TERMINATING...");
+                System.exit(0);
+            }
         }
     }
 
-
+    private void processAssign(Node node) throws IOException{
+        symbolTable.lookup(parseName(node.jjtGetChild(0).toString()), Descriptor.Type.VAR);
+    }
     private void processProgram(Node node){
         symbolTable.enterScopeForAnalysis(); // Enter Import Scope
         for (int i = 0; i < node.jjtGetNumChildren()-1; i++) { // Imports
