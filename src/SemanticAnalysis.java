25cadcf1dbc388ce5199cc8cee3110d1c6c03d88
joanaferreira0011 <joanaferreira0011@gmail.com>
2020-04-29 14:54:56 +0100

    changed

--- src/SemanticAnalysis.java
+++ src/SemanticAnalysis.java
@@ -50,7 +50,7 @@ class SemanticAnalysis{
             }
         }catch (Exception e) {
             System.err.println("[SEMANTIC ERROR]: " + e.getMessage());
-            e.printStackTrace();
+            // e.printStackTrace();
             exceptionCounter++;
             if (exceptionCounter >= MAX_EXCEPTIONS) {
                 System.err.println("[PROGRAM TERMINATING] THERE ARE MORE THAN " + MAX_EXCEPTIONS + " SEMANTIC ERRORS.");
