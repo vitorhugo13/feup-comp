e3e5e830793f5f2779b9b1f368b1fcd6cc876c95
joanaferreira0011 <joanaferreira0011@gmail.com>
2020-06-07 21:46:39 +0100

    commented symbol table

--- src/SymbolTable.java
+++ src/SymbolTable.java
@@ -4,6 +4,13 @@ import java.util.Stack;
 import java.util.ArrayList;
 import java.io.IOException;
 
+/**
+ * Symbol Table
+ *
+ * This class builds a symbol table that can be used for a semantic analysis or/and to generate code.
+ * It will store the symbols in several hash maps representing scopes, connected to each other.
+ * There are several extra data structures such as a stack and an ArrayList that are used to make the code cleaner and more efficient.
+ */
 public class SymbolTable{
 
     private Stack<MyHashMap> stack;
@@ -19,6 +26,9 @@ public class SymbolTable{
         return this.className;
     }
 
+    /**
+     * Should be called after every analysis to reset the table.
+     */
     public void reset() {
         this.posArrayForAnalysis = 0;
     }
@@ -34,7 +44,9 @@ public class SymbolTable{
 
     }
 
-    
+    /**
+     * Should be called when building the symbol table to create and enter a new scope.
+     */
     public void enterScope() {
 
         MyHashMap myHash = new MyHashMap(stack.peek());
@@ -44,6 +56,9 @@ public class SymbolTable{
 
     }
 
+    /**
+     * After the table being build, this function should be called during an analysis, to enter a new scope.
+     */
     public void enterScopeForAnalysis() {
         
         stack.push(all_hashes.get(posArrayForAnalysis));
@@ -51,6 +66,9 @@ public class SymbolTable{
         // System.out.println("[SCOPE] Enter scope for analysis: " + stack.peek());
     }
 
+    /**
+     * @return ArrayList of VarDescriptors representing the class attributes
+     */
     public ArrayList<VarDescriptor> getClassAtributes(){
         ArrayList<VarDescriptor> variables = new ArrayList<>();
         for(ArrayList<Descriptor> value : all_hashes.get(1).getHash().values()){
@@ -61,6 +79,9 @@ public class SymbolTable{
         return variables;
     }
 
+    /**
+     * Should be called to indicate leaving the current scope, when building the symbol table
+     */
     public void exitScope() {
         //System.out.println("[SCOPE] Exit scope: " + stack.peek());
         if (stack.empty()) {
@@ -70,12 +91,19 @@ public class SymbolTable{
         stack.pop();
     }
 
+    /**
+     * Should be called to indicate leaving the current scope, when doing an analysis after the table being built
+     */
     public void exitScopeForAnalysis() {
         // System.out.println("[SCOPE] Exit scope for analysis: " + stack.peek());
         stack.pop();
     }
 
-
+    /**
+     * Adds a new symbol to the Symbol Table
+     * @param id identifier of the descriptior to be added to the class. This will be used in the lookup function to search
+     * @param info descriptor with the symbol information
+     */
     public void add(String id, Descriptor info) {
         
         if (stack.empty()) {
@@ -97,6 +125,14 @@ public class SymbolTable{
         (stack.peek()).add(id, info);
     }
 
+    /**
+     * Adds a new symbol to the Symbol Table
+     * should only be used for import statements as imported methods are stored inside the Class descriptors they belong,
+     * therefore this will create a class if it doesn't exist already
+     * @param id - name of the class to be added
+     * @param info -
+     * @param isImport - this is only to allow an overlad of the method add
+     */
     public void add(String id, MethodDescriptor info, Boolean isImport) {
         if (stack.empty()) {
             System.err.println("[ADD]: can't add a symbol without a scope.");
@@ -114,7 +150,17 @@ public class SymbolTable{
     }
 
 
-
+    /**
+     * Searches for a symbol in the symbol table
+     * @param id - Identifier of the symbol to search
+     * @return ArrayList<Descriptor> with the descriptors corresponding to the identifier given.
+     * It will look for the closest descriptor:
+     * 1. In the current scope
+     * 2. In the parent scopes
+     * 3. In the import scope
+     * 4. If the class expands another, it will look in the methods of that class
+     * @throws IOException
+     */
     public ArrayList<Descriptor>  lookup(String id) throws IOException {
 
         if (stack.empty()) {
@@ -149,6 +195,9 @@ public class SymbolTable{
         return result;
     }
 
+    /**
+     * Prints the table in a readable form
+     */
     public void print_all(){
 
         for(int i = 0; i < all_hashes.size(); i++){
@@ -180,6 +229,10 @@ public class SymbolTable{
         }
     }
 
+    /**
+     *
+     * @return the current type of scope
+     */
     public Descriptor.Scope getCurrentScope(){
         switch(stack.size()){
             case 1:
